4db60d6c8690be1be212ddaf135ba5075fb566ad
Tom Glenn <tommyg78@gmail.com>
2018-10-14 11:45:43 -0500

    Make Functional_tests an app, use LiveUserTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,12 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):  
+class NewVisitorTest(LiveServerTestCase):
 
-    def setUp(self):  
+    def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):  
@@ -19,11 +19,11 @@ class NewVisitorTest(unittest.TestCase):
 
 
   
-    def test_can_start_a_list_and_retrieve_it_later(self):  
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
-
+        self.browser.get(self.live_server_url)
+        
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text  
